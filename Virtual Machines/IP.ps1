a3ec2167820cef12f8ef8fde66eff8ba37e00c89
Phil Hunt <philiphuntiv2@gmail.com>
2025-11-26 15:12:54 -0500

    Update IP.ps1

--- Virtual Machines/IP.ps1
+++ Virtual Machines/IP.ps1
@@ -3,21 +3,17 @@ param(
     [string] $Name
 )
 
-try {
+$NetAdapter = (Get-VM `
+    -VMName $Name `
+    | Select-Object -ExpandProperty NetworkAdapters `
+)
 
-    $NetAdapter = (Get-VM `
-        -VMName $Name `
-        | Select-Object -ExpandProperty NetworkAdapters `
-    )
+$IP = $NetAdapter.IPAddresses `
+    | Where-Object {$_ -notlike '*::*'} `
+    | Select-Object -First 1
 
-    $IP = $NetAdapter.IPAddresses `
-        | Where-Object {$_ -notlike '*::*'} `
-        | Select-Object -First 1
-
+if ($IP) {
     $IP | ConvertTo-Json | Write-Output
-
-} catch {
-
-    $null | ConvertTo-Json | Write-Output
-
+} else {
+    Write-Output 'null'
 }
