41fa26ae763f45657a47d9f04c8297fe6329af02
Phil Hunt <philiphuntiv2@gmail.com>
2025-11-26 07:37:20 -0500

    will hide output

--- Virtual Machines/Save.ps1
+++ Virtual Machines/Save.ps1
@@ -1,3 +1,8 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6159522e3682f4c0a409a380596b90bb0c2d68c6eb67aa00da600a10a216b3ba
-size 66
+param(
+    [string] $Name
+)
+
+Stop-VM `
+    -Name $Name `
+    -Save `
+    > $null
--- Virtual Machines/Start.ps1
+++ Virtual Machines/Start.ps1
@@ -1,3 +1,8 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:97439bba2b5a7a1176dcb838d9c9eca216af19f64a835dd9a4989c3be053de9e
-size 50
+
+param(
+    [string] $Name
+)
+
+Start-VM `
+    -Name $Name `
+    > $null
