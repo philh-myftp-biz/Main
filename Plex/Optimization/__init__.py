391fb1c1b9047f7a2e4b33e623dc9f334710ea31
Phil Hunt <philiphuntiv2@gmail.com>
2025-11-30 15:20:21 -0500

    Modified

--- Plex/Optimization/__init__.py
+++ Plex/Optimization/__init__.py
@@ -1,10 +1,8 @@
-from ffmpeg_progress_yield import FfmpegProgress
 from philh_myftp_biz.modules import Module, ModuleLockedError
 from philh_myftp_biz.file import ZIP, temp
 from philh_myftp_biz.web import download
 from philh_myftp_biz import ParsedArgs
 from philh_myftp_biz.pc import Path
-from tqdm import tqdm
 
 #==============================================
 # Parse commandline arguements
@@ -34,75 +32,34 @@ class QueueItem:
         src: Path
     ):
         self.src = src
+        self.tmp = temp('encoding', 'mp4')
         self.dst = src.chext('mp4')
 
 #==============================================
 # FFMPEG
 
+# Ffmpeg.exe
 ffmpeg = temp('ffmpeg', 'exe', '0')
 
-#
+# If ffmpeg.exe does not exist
 if not ffmpeg.exists():
 
-    #
+    # ffmpeg.zip
     zipfile = temp('ffmpeg', 'zip')
 
-    #
+    # Download ffmpeg.zip
     download(
         url = 'https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip',
         path = zipfile
     )
 
-    #
+    # Wrap ffmpeg.zip
     zip = ZIP(zipfile)
 
-    #
+    # Extract ffmpeg.exe from ffmpeg.zip
     zip.extractFile(
         file = next(zip.search('ffmpeg.exe')),
         path = ffmpeg
     )
-
-#==============================================
-# ENCODER
-
-def encode(i:QueueItem):
-
-    try:
-
-        cmd = [
-
-            str(ffmpeg), # Ffmpeg.exe
-            
-            '-hwaccel', 'auto', # Use GPU
-
-            '-i', str(i.src), # Input Path
-            
-            '-map', f'0:v:0', # Video Stream
-            '-c:v', 'h264_nvenc', # Video Codec
-
-            '-map', f'0:a:m:language:eng', # Audio Stream
-            '-c:a', 'copy', # Audio Codec
-
-            str(i.dst), # Output Path
-            '-y', # Overwrite Existing File
-
-        ]
-
-        pbar = tqdm(
-            total = 100,
-            position = 1,
-            desc = "Encoding"
-        )
-        
-        ff = FfmpegProgress(cmd)
-
-        for progress in ff.run_command_with_progress():
-            pbar.update(progress - pbar.n)
-
-    except RuntimeError as e:
-
-        stderr: str = e.args[0]
-
-        raise RuntimeError(stderr.strip()) from None
 
 #==============================================
--- Plex/Optimization/run.py
+++ Plex/Optimization/run.py
@@ -1,43 +1,77 @@
-from __init__ import this, QueueItem, encode, args
+from __init__ import this, QueueItem, args, ffmpeg
+from ffmpeg_progress_yield import FfmpegProgress
 from philh_myftp_biz.db import MimeType
+from tqdm import tqdm
 
 #
 queue: list[QueueItem] = []
 
-#
+# Iter through descendants of 'E:/Plex/Media/'
 for src in this.dir.child('/Media/').descendants():
 
-    #
+    # If the download limit is reached
     if len(queue) == args['limit']:
         break
 
-    #
-    elif MimeType.Path(src) == 'video':
+    # If the path is a video, but not '.mp4'
+    elif (MimeType.Path(src) == 'video') and (src.ext() != 'mp4'):
 
-        #
-        if src.ext() != 'mp4':
+        # Append the item to the queue
+        queue += [QueueItem(src)]
 
-            queue += [QueueItem(src)]
-
-#
+# Iter through all items in the queue
 for i in queue:
     
-    #
+    # Print the source and destination paths
     print()
     print(i.src)
+    print(i.tmp)
     print(i.dst)
 
     try:
 
         #
-        encode(i)
+        ff = FfmpegProgress([
+
+            str(ffmpeg), # Ffmpeg.exe
+            
+            '-hwaccel', 'auto', # Use GPU
+
+            '-i', str(i.src), # Input Path
+            
+            '-map', f'0:v:0', # Video Stream
+            '-c:v', 'h264_nvenc', # Video Codec
+
+            '-map', f'0:a:m:language:eng', # Audio Stream
+            '-c:a', 'copy', # Audio Codec
+
+            str(i.tmp), # Output Path
+
+            '-y', # Overwrite Existing File
+
+        ])
+
+        # Progress Bar
+        pbar = tqdm(
+            total = 100,
+            position = 1,
+            desc = "Encoding"
+        )
+        
+        #
+        for progress in ff.run_command_with_progress():
+            
+            #
+            pbar.update(progress - pbar.n)
+        
+        #
+        i.tmp.move(i.dst)
 
         #
         i.src.delete()
 
-    #
-    except Exception as e:
+    except RuntimeError as e:
 
-        i.dst.delete()
+        stderr: str = e.args[0]
 
-        raise e
+        raise RuntimeError(stderr.strip()) from None
