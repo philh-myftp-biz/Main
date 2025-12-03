5bfd453bfb02efefe23685d88e5a5cb5fa4a8c0f
Phil Hunt <philiphuntiv2@gmail.com>
2025-12-03 17:38:35 -0500

    Update run.py

--- Minecraft/Update/run.py
+++ Minecraft/Update/run.py
@@ -1,15 +1,15 @@
-from __init__ import Server, this, File
+from __init__ import World, this, File
 from philh_myftp_biz.json import Dict
 from philh_myftp_biz.file import INI
 import Files
 
 for p in this.dir.child('/Worlds/'):
     
-    # Wrap the server path
-    server = Server(p)
+    # Wrap the world
+    w = World(p)
 
     # Get a list of files to copy
-    files: list[File] = getattr(Files, server.edition)
+    files: list[File] = getattr(Files, w.edition)
 
     # Iter through all files for this edition
     for f in files:
@@ -18,35 +18,35 @@ for p in this.dir.child('/Worlds/'):
         src = f.path
 
         # Destination File
-        dst = server.path.child(f.name)
+        dst = w.path.child(f.name)
 
         # Copy the src file to the dst path
         src.copy(dst)
 
     # If the edition is java
-    if server.edition == 'java':
+    if w.edition == 'java':
         
         # Wrap the 'server.properties' file
-        props = Dict(INI(server.path.child('server.properties')))
+        props = Dict(INI(w.path.child('server.properties')))
 
         # Option: difficulty
-        props['difficulty'] = server.config['difficulty']
+        props['difficulty'] = w.config['difficulty']
 
         # Option: cheats
-        props['enable-command-block'] = server.config['cheats']
+        props['enable-command-block'] = w.config['cheats']
 
         # Option: gamemode
-        props['gamemode'] = server.config['gamemode']
+        props['gamemode'] = w.config['gamemode']
 
         # Option: players
-        props['max-players'] = server.config['players']
+        props['max-players'] = w.config['players']
 
         # Option: message
-        props['motd'] = server.config['message']
+        props['motd'] = w.config['message']
 
         # Option: pvp
-        props['pvp'] = server.config['pvp']
+        props['pvp'] = w.config['pvp']
 
         # Option: port/java
-        props['server-port'] = server.config['port']['java']
+        props['server-port'] = w.config['port']['java']
 
