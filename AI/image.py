971563ca71b6c73cdc5e318dcb22128f35334452
Phil Hunt <philiphuntiv2@gmail.com>
2025-12-07 16:20:18 -0500

    Update image.py

--- AI/image.py
+++ AI/image.py
@@ -1,14 +1,34 @@
 from __init__ import args, PipeLine, messages
-from philh_myftp_biz.file import temp
+from philh_myftp_biz.text import random
+from philh_myftp_biz.pc import Path
 
 # ====================================================
 # PARSE INPUT
 
 args.Arg(
     name = 'model',
-    default = 'runwayml/stable-diffusion-v1-5',
-    desc = '',
-    handler = str
+    default = 'runwayml/stable-diffusion-v1-5'
+)
+
+args.Arg(
+    name = 'path',
+    default = Path(f'E:/__temp__/{random(15)}.png'),
+    desc = 'Path to save image',
+    handler = Path
+)
+
+args.Arg(
+    name = 'width',
+    default = 512,
+    
+    handler = lambda x: int(x)//8 * 8
+)
+
+args.Arg(
+    name = 'height',
+    default = 512,
+    
+    handler = lambda x: int(x)//8 * 8
 )
 
 # ====================================================
@@ -17,17 +37,21 @@ args.Arg(
 # Initialize the pipeline
 pipeline = PipeLine(args['model'])
 
-# Temporary path for output image
-imgfile = temp('ai_image', 'png')
+# Path for output image
+imgfile: Path = args['path']
 
 # Prompt the pipeline
-prompt = pipeline(messages.prompt())
+prompt = pipeline(
+    prompt = messages.prompt(),
+    height = args['height'],
+    width = args['width']
+)
 
 # Save the generated image
 prompt.images[0].save(str(imgfile))
 
 # Upload the image to the messages object
-messages.add_file(imgfile)
+messages.add_file('assistant', imgfile)
 
 # ====================================================
 
