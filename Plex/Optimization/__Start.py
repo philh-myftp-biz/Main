9e597cb3634958b42eb37266b2b130bf10945aef
Phil Hunt <philiphuntiv2@gmail.com>
2025-12-21 16:01:17 -0500

    Repaired Optimization Scripts
    Auto English Stream Selection still problematic

--- Plex/Optimization/__Start.py
+++ Plex/Optimization/__Start.py
@@ -1,6 +1,8 @@
-from __init__ import this, QueueItem, args, ffmpeg, PIDstore
+from __init__ import this, QueueItem, ffmpeg, PIDstore
 from ffmpeg_progress_yield import FfmpegProgress
 from philh_myftp_biz.db import MimeType
+from philh_myftp_biz.classOBJ import log
+from philh_myftp_biz.pc import cls
 from tqdm import tqdm
 from os import getpid
 
@@ -13,24 +15,27 @@ queue: list[QueueItem] = []
 # Iter through descendants of 'E:/Plex/Media/'
 for src in this.dir.child('/Media/').descendants():
 
-    # If the download limit is reached
-    if len(queue) == args['limit']:
-        break
-
     # If the path is a video, but not '.mp4'
-    elif (MimeType.Path(src) == 'video') and (src.ext() != 'mp4'):
+    if (MimeType.Path(src) == 'video') and (src.ext() != 'mp4'):
 
         # Append the item to the queue
         queue += [QueueItem(src)]
 
+        print(src)
+
+#
+queue.sort(
+    key = lambda i: i.size
+)
+
 # Iter through all items in the queue
 for i in queue:
     
+    #
+    cls()
+
     # Print the source and destination paths
-    print()
-    print(i.src)
-    print(i.tmp)
-    print(i.dst)
+    log(i) 
 
     try:
 
@@ -43,15 +48,13 @@ for i in queue:
 
             '-i', str(i.src), # Input Path
             
-            '-map', f'0:v:0', # Video Stream
-            '-c:v', 'h264_nvenc', # Video Codec
+#            '-map', f'0:v:0', # Video Stream
+#            '-c:v', 'h264_nvenc', # Video Codec
 
-            '-map', f'0:a:m:language:eng', # Audio Stream
-            '-c:a', 'copy', # Audio Codec
+#            '-map', f'0:a:{i.streams['audio']['eng']}', # Audio Stream
+#            '-c:a', 'copy', # Audio Codec
 
             str(i.tmp), # Output Path
-
-            '-y', # Overwrite Existing File
 
         ])
 
--- Plex/Optimization/__init__.py
+++ Plex/Optimization/__init__.py
@@ -1,13 +1,9 @@
 from philh_myftp_biz.file import ZIP, temp, TXT
 from philh_myftp_biz.modules import Module
 from philh_myftp_biz.web import download
-from philh_myftp_biz import ParsedArgs
 from philh_myftp_biz.pc import Path
-
-#==============================================
-# Parse commandline arguements
-args = ParsedArgs()
-args.Arg('limit', 5)
+from philh_myftp_biz import run
+from typing import Literal
 
 #==============================================
 # Plex Module
@@ -22,12 +18,60 @@ PIDstore = TXT(this.dir.child('/Optimization/__pycache__/PID.txt'))
 
 class QueueItem:
 
+    streams: dict[str, dict[str, int]] = {
+
+        'audio': {},
+
+        'video': {},
+
+        'subtitle': {}
+
+    }
+
     def __init__(self,
         src: Path
     ):
+        # ==========================================
+
         self.src = src
-        self.tmp = temp('encoding', 'mp4')
         self.dst = src.chext('mp4')
+
+        self.tmp = temp('encoding', 'mp4')
+
+        self.size = src.size()
+
+        # ==========================================
+        """
+        r = run(
+            args = [
+                ffprobe,
+                '-v', 'quiet',
+                '-print_format', 'json',
+                '-show_streams',
+                '-show_format', 
+                self.src
+            ],
+            wait = True,
+            hide = True
+        )
+
+        streams: dict[str, dict[str, str]] = r.output('json')['streams']
+
+        for stream in streams:
+
+            if 'tags' in stream:
+            
+                if 'language' in stream['tags']:
+
+                    lang: str = stream['tags']['language']
+
+                    type: str = stream['codec_type']
+
+                    index = stream['index']
+
+                    self.streams[type][lang] = index
+        """
+        # ==========================================
 
 #==============================================
 # FFMPEG
@@ -35,8 +79,11 @@ class QueueItem:
 # Ffmpeg.exe
 ffmpeg = temp('ffmpeg', 'exe', '0')
 
+#
+ffprobe = temp('ffprobe', 'exe', '0')
+
 # If ffmpeg.exe does not exist
-if not ffmpeg.exists():
+if not (ffmpeg.exists() and ffprobe.exists()):
 
     # ffmpeg.zip
     zipfile = temp('ffmpeg', 'zip')
@@ -54,6 +101,12 @@ if not ffmpeg.exists():
     zip.extractFile(
         file = next(zip.search('ffmpeg.exe')),
         path = ffmpeg
+    )
+
+    #
+    zip.extractFile(
+        file = next(zip.search('ffprobe.exe')),
+        path = ffprobe
     )
 
 #==============================================
