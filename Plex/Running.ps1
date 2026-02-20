a5531b9bc98d555ca2c5ab069da943a7eb7f6471
Phil Hunt <philiphuntiv2@gmail.com>
2026-02-20 08:54:04 -0500

    Update Running.ps1

--- Plex/Running.ps1
+++ Plex/Running.ps1
@@ -1,6 +1,16 @@
 
+# Store the current progress preference
+$OriginalProgressPreference = $ProgressPreference 
+
+# Set the progress preference to 'SilentlyContinue' to hide progress
+$ProgressPreference = 'SilentlyContinue'
+
+# Test the port
 Test-NetConnection `
     -ComputerName localhost `
     -Port 32400 `
     -InformationLevel Quiet `
     | ConvertTo-Json | Write-Host
+
+# Restore the original progress preference setting
+$ProgressPreference = $OriginalProgressPreference 
