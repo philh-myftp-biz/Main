5b87f25ee2ec6494944ff3486f5d1f521e08ea0d
Phil Hunt <philiphuntiv2@gmail.com>
2026-03-14 16:34:46 -0400

    Update Create.ps1

--- Users/Create.ps1
+++ Users/Create.ps1
@@ -10,23 +10,42 @@ Import-Module "$PSScriptRoot\mod.psm1" -Function Repair-User
 
 #=====================================================================================================
 
-# Create new Active Directory User
-New-ADUser `
-    -Name $Username `
-    -GivenName $FirstName `
-    -Surname $LastName
+$ADUser = Get-ADUser `
+    -Identity $Username `
+    -ErrorAction SilentlyContinue
+
+# If user does not exist
+if ($null -eq $ADUser) {
+
+    # Create new Active Directory User
+    New-ADUser `
+        -Name $Username `
+        -GivenName $FirstName `
+        -Surname $LastName `
+        -Verbose
+
+# If user does exist
+} else {
+
+    Set-ADUser `
+        -Identity $Username `
+        -GivenName $FirstName `
+        -Surname $LastName `
+        -Verbose
+
+}
 
 # Check if password is given
 if ($null -ne $Password) {
 
-    #
     Set-ADAccountPassword `
         -Identity $Username `
-        -NewPassword $Password
+        -NewPassword $Password `
+        -Verbose
 
-    #
     Enable-ADAccount `
-        -Identity $Username
+        -Identity $Username `
+        -Verbose
 
 }
 
